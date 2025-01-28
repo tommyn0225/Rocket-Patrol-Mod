a2d09ec770a21f720e4acc137056497670e091d1
tommyn0225 <tommyn0225@gmail.com>
2025-01-27 18:23:20 -0800

    add documentation for fastship, timing

--- src/main.js
+++ src/main.js
@@ -1,4 +1,21 @@
 //src/main.js
+
+/* Tommy Nguyen
+Rocket Patrol Modded
+~6 hours(?)
+
+Total points (20)
+Create a new enemy Spaceship type (w/ new artwork) that's smaller, moves faster, and is worth more points (5)
+- created a new sprite on Piskelapp and built off the logic of Spaceship.js to create Fastship.js and making
+it smaller, faster, and worth more points
+
+Implement a new timing/scoring mechanism that adds time to the clock for successful hits and subtracts time for misses (5)
+- built off the logic of the timer system, displaying it for testing purposing and adding 2 seconds for each time that
+shipExplode() executes. The update() in Rocket.js handles misses, subtracting 5 seconds everytime the rocket resets on miss
+
+
+*/
+
 let config = {
     type: Phaser.AUTO,
     width: 640,
